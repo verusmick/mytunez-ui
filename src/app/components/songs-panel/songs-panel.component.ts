31525180cedd991bbe72853b604f17ee09433784
verusmick <miguel.angel.angulo72@gmail.com>
2020-06-11 13:15:35 -0400

    Add play Button and hover effects

--- src/app/components/songs-panel/songs-panel.component.ts
+++ src/app/components/songs-panel/songs-panel.component.ts
@@ -12,18 +12,39 @@ export class SongsPanelComponent implements OnInit {
   @Input() artist;
   serverPath: string = AppSettings.API_ENDPOINT + 'src/song/';
 
-
   constructor(private songsService: SongsService) { }
 
   ngOnInit(): void { }
 
   watchArtist(change) {
     this.songsService.getSongs(change.currentValue.artist_id).subscribe((res: any) => {
-      this.songs = res.data.requests;
+      let songsList = res.data.requests;
+      for (let i = 0; i < songsList.length; i++) {
+        songsList[i]['isPlaying'] = false;
+        songsList[i]['lengthInMinutes'] = Math.floor(songsList[i]['length'] / 60);
+        songsList[i]['hoverSong'] = false;
+      }
+      this.songs = songsList
     })
   }
 
   ngOnChanges(changes: SimpleChanges) {
     this.watchArtist(changes.artist);
+  }
+  playSong(song) {
+    for (let i = 0; i < this.songs.length; i++) {
+      if (song.song_id === this.songs[i].song_id) {
+        this.songs[i].isPlaying = !song.isPlaying;
+      } else {
+        this.songs[i].isPlaying = false
+      }
+    }
+  }
+
+  showPlayerIcon(event, song) {
+    song.hoverSong = true;
+  }
+  hidePlayerIcon(event, song) {
+    song.hoverSong = false;
   }
 }
