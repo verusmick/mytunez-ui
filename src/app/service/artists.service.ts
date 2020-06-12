c4c54d89999749e349671f9077d66ab0227f1296
verusmick <miguel.angel.angulo72@gmail.com>
2020-06-12 13:44:00 -0400

    Add Api Key in the enpoints

--- src/app/service/artists.service.ts
+++ src/app/service/artists.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AppSettings } from '../app.config'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistsService {
+  headers = new  HttpHeaders().set("api-key", 'mytunez-tk');
 
   constructor(private http: HttpClient) { }
 
   getArtists() {
-    return this.http.get(AppSettings.API_ENDPOINT + 'artists');
+    return this.http.get(AppSettings.API_ENDPOINT + 'artists', {headers:this.headers});
   }
-  postArtist(newArtist:any) {
-    return this.http.post(AppSettings.API_ENDPOINT + 'artists', newArtist);
+  postArtist(newArtist:any) { 
+    return this.http.post(AppSettings.API_ENDPOINT + 'artists', newArtist,{headers:this.headers});
   }
 }
--- src/app/service/songs.service.ts
+++ src/app/service/songs.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AppSettings } from '../app.config'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SongsService {
+  headers = new HttpHeaders().set("api-key", 'mytunez-tk');
 
   constructor(private http: HttpClient) { }
 
   getSongs(artist: string) {
-    return this.http.get(AppSettings.API_ENDPOINT + `songs?artist=${artist}`);
+    return this.http.get(AppSettings.API_ENDPOINT + `songs?artist=${artist}`, { headers: this.headers });
   }
 
   postSong(newSong: any) {
-    return this.http.post(AppSettings.API_ENDPOINT + 'songs', newSong);
-  }  
+    return this.http.post(AppSettings.API_ENDPOINT + 'songs', newSong, { headers: this.headers });
+  }
 }
